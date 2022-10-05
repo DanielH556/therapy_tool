ee0975739dc682ce87609ae81ebfa7281302fd55
Daniel Eiji Hattori Hossaki <daniel.hossaki@outlook.com>
2022-10-05 16:33:38 -0300

    Login form and Auth tests

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,17 +1,26 @@
+// Importação de Bibliotecas
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// Importação de Componentes e Páginas
 import TestPage from './pages/TestPage';
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
+import Profile from './pages/Profile';
+import LoginForm from './pages/LoginForm';
+import LoginForm2 from './pages/LoginForm2';
 
 export default function Router() {
    return(
       <BrowserRouter>
          <Routes>
+            {/* Rotas para cada página */}
             <Route path="/" element={<Login />} />
             <Route path="/testPage" element={<TestPage />} />
             <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/logintest" element={<LoginForm />} />
+            <Route path="/logintesttwo" element={<LoginForm2 />} />
          </Routes>
       </BrowserRouter>
    )
