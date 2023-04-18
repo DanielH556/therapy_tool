573a14b3862d5f4034d1098ec65ec603baf66fde
Meblendorius <lemos.c.thiago@gmail.com>
2023-04-18 01:49:24 +0000

    server adjustments

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import PatientController from './controllers/PatientController';
+import ApiController from './controllers/ApiController';
 
 const routes = Router();
 
@@ -8,5 +9,7 @@ routes.get('/paciente', PatientController.index);
 routes.get('/paciente/:id', PatientController.show)
 routes.post('/novoPaciente', PatientController.create)
 routes.delete('/deletePaciente/:id', PatientController.delete)
+routes.get('/ultimosDias/:id', ApiController.ultimosDias)
+routes.get('/wordcloud/:id', ApiController.wordcloud)
 
 export default routes;
